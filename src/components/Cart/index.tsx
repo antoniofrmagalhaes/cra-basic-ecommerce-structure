9774abe01ff26b0afe4413464323a5fec084b58d
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-11-02 00:24:55 -0400

    feat: new cart component created using global slider

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,58 +1,43 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
-import { RiCloseLine, RiShoppingCart2Line } from 'react-icons/ri';
-import OutsideClickHandler from 'react-outside-click-handler';
-import { useCart } from '../../contexts/CartContext';
+
+import Slider from '../Slider';
+
 import ProductCard from './ProductCard';
 
+import { useCart } from '../../contexts/CartContext';
+
 import {
-  Overlay,
-  Container,
-  Header,
-  Content,
   ListContainer,
-  SubtotalContainer,
-  CheckoutButton,
   ListHeader,
   List,
+  SubtotalContainer,
+  CheckoutButton,
 } from './styles';
 
 const Cart: React.FC = () => {
-  const { cart, subtotal, isOpen, handleCloseCart } = useCart();
+  const { cart, subtotal, handleCloseCart } = useCart();
   return (
-    <Overlay isOpen={isOpen}>
-      <OutsideClickHandler onOutsideClick={handleCloseCart}>
-        <Container isOpen={isOpen}>
-          <Header>
-            <RiCloseLine size={28} onClick={handleCloseCart} />
-            <p>Carrinho de Compras</p>
-            <RiShoppingCart2Line size={22} />
-          </Header>
-          <Content>
-            <ListContainer>
-              <ListHeader>
-                <p>Meus Itens</p>
-              </ListHeader>
-              <List>
-                {cart.products.map(p => (
-                  <ProductCard key={p._id} product={p} />
-                ))}
-              </List>
-            </ListContainer>
-            <SubtotalContainer>
-              <div>
-                <p>Subtotal:</p>
-                <strong>{subtotal}</strong>
-              </div>
-            </SubtotalContainer>
-            <CheckoutButton to="/cart" onClick={handleCloseCart}>
-              Finalizar Pedido
-            </CheckoutButton>
-          </Content>
-        </Container>
-      </OutsideClickHandler>
-    </Overlay>
+    <Slider>
+      <ListContainer>
+        <ListHeader>
+          <p>Meus Itens</p>
+        </ListHeader>
+        <List>
+          {cart.products.map(p => (
+            <ProductCard key={p._id} product={p} />
+          ))}
+        </List>
+      </ListContainer>
+      <SubtotalContainer>
+        <div>
+          <p>Subtotal:</p>
+          <strong>{subtotal}</strong>
+        </div>
+      </SubtotalContainer>
+      <CheckoutButton to="/cart" onClick={handleCloseCart}>
+        Finalizar Pedido
+      </CheckoutButton>
+    </Slider>
   );
 };
 
--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,80 +1,5 @@
 import { Link } from 'react-router-dom';
-import styled, { css, keyframes } from 'styled-components';
-
-interface IContainerProps {
-  isOpen: boolean;
-}
-
-const fromRight = keyframes`
-  0% {
-    transform: translateX(100%);
-  }
-  100% {
-    transform: translateX(0);
-  }
-`;
-
-export const Overlay = styled.div<IContainerProps>`
-  z-index: 3;
-  display: none;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background: #ffffff;
-  ${({ isOpen }) =>
-    isOpen &&
-    css`
-      display: block;
-    `}
-  @media (min-width: 769px) {
-    background: rgba(0, 0, 0, 0.5);
-  }
-`;
-
-export const Container = styled.div<IContainerProps>`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  transform: translateX(100%);
-  background: #ffffff;
-  ${({ isOpen }) =>
-    isOpen &&
-    css`
-      animation: 350ms ${fromRight} 150ms ease forwards;
-    `}
-  @media(min-width: 769px) {
-    width: 25%;
-  }
-`;
-
-export const Header = styled.div`
-  width: 100%;
-  height: 8.5%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 16px;
-  border-bottom: 1px solid #dddddd;
-  color: white;
-  background: #037ffc;
-  > p {
-    letter-spacing: 2px;
-    font-weight: 400;
-  }
-`;
-
-export const Content = styled.div`
-  width: 100%;
-  height: 91.5%;
-  display: flex;
-  flex-direction: column;
-  padding: 16px;
-`;
+import styled from 'styled-components';
 
 export const ListContainer = styled.div`
   width: 100%;
@@ -107,7 +32,6 @@ export const SubtotalContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    font-size: 15px;
     > p {
       display: flex;
       align-items: center;
@@ -125,6 +49,6 @@ export const CheckoutButton = styled(Link)`
   align-items: center;
   border-radius: 4px;
   border: 0;
-  color: white;
-  background: #41b02e;
+  color: ${({ theme }) => theme.colors.secondaryText};
+  background: ${({ theme }) => theme.colors.secondary};
 `;
